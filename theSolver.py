108f58c3c30b08b2d98bcba57e195a0380fbde49
bilan604 <77251582+bilan604@users.noreply.github.com>
2022-03-31 18:41:07 -0700

    Add files via upload

--- theSolver.py
+++ theSolver.py
@@ -1,3 +1,12 @@
+"""
+Make the non-symetric nxn matrix A corresponding to an adjacency matrix
+"""
+n = len(A)
+
+# the damping parameter / beta
+p = 0.85
+not0 = (1-p)/float(n)
+
 # the iterative solver's "Link"
 class Link(object):
     def __init__(self, ind):
@@ -7,7 +16,7 @@ class Link(object):
         self.page_rank = 0.1
 
     def iterate(self):
-        self.page_rank = p * sum([link.page_rank/len(link.ri) for link in self.ci]) + ri_not0
+        self.page_rank = p * sum([link.page_rank/len(link.ri) for link in self.ci]) + not0
 
 # the iterative solver's "Matrix"
 class Matrix(object):
@@ -27,3 +36,17 @@ class Matrix(object):
                     link.ri.append(self.index_ref[i])
                 if col[i] == 1:
                     link.ci.append(self.index_ref[i])
+
+# run n iterations
+def run_iterations(mtx, n):
+    for i in range(n):
+        for link in mtx.links:
+            link.iterate()
+
+matrix = Matrix([ind for ind in range(n)])
+matrix.initialize()
+
+start = default_timer()
+run_iterations(matrix, 50)
+stop = default_timer()
+print(f'Iterative solution took: {stop - start}s')
